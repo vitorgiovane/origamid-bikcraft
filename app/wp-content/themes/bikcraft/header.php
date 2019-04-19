47d25308ee6bd483a5e27b4748a251bbeb7b481c
Vitor <vitorgpalves@gmail.com>
2019-04-19 16:04:00 -0300

    feature(global): adiciona nome dinâmico ao título
    O título do site estava definido estaticamente, agora por uma função
    PHP.
    
    ===
    T3

--- app/wp-content/themes/bikcraft/header.php
+++ app/wp-content/themes/bikcraft/header.php
@@ -3,11 +3,11 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Bikcraft - Custom bicycles</title>
+  <title><?php bloginfo('name'); ?></title>
   <meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
 
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="Bikcraft - Bicicletas Personalizadas" />
+  <meta property="og:title" content="<?php bloginfo('name'); ?>" />
   <meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte." />
   <meta property="og:url" content="http://bikcraft.com" />
   <meta property="og:image" content="http://bikcraft.com/img/og-image.png" />
